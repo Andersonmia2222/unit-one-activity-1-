29e6c20d27cc8843ce02589e773d939856a99e7f
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-09-16 14:55:47 +0000

    added scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+// 1.import scanner class 
+import java.util.Scanner;
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Lets play Madlibs!");
+      // 2. set up a scanner object
+      Scanner input = new Scanner(System.in);
+
+      //System.in refers to the physical keyboard!
+
+      // 3. use the scanner to take input
+      System.out.println("Mia");
+      String player = input.nextLine();
+      System.out.println("welcome," + player);
+
+
    }
 }
