cbfcc9bf9b1abcbc6358834695b6fa81da30611a
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-09-18 18:23:45 +0000

    madlib

--- Main.java
+++ Main.java
@@ -14,6 +14,33 @@ public class Main {
       String player = input.nextLine();
       System.out.println("welcome," + player);
 
+      public class Story {
+    public static void main(String[] args) {
+        String emotion = "tired";
+        // print your story line by line
+        System.out.println("I woke up feeling " + emotion + " today.");
 
-   }
+        String adjective = "delicious";
+        System.out.println("I walked into my house to the " + adjective + " smell of Christmas.");
+
+        String roomInHouse = "kitchen";
+        System.out.println("I walked into the " + roomInHouse + " to the smell of dinner.");
+         String Noun1 = "People";
+      System.out.println( People + "were coming over for dinner ");
+      String Noun2 = "fun";
+       System.out.println( "We had so much" + fun);
+
+
+
+
+
+
+
+    }
 }
+
+
+
+   
+
+
